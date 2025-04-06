3e82e5d0e77e8c7db29cd56611e1f0299c158b44
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-04-06 12:45:28 +0200

    refactor(demo-cleanup): удалены демонстрационные тексты и CSS-настройки
    Удалены временные демонстрационные элементы, включая тексты-заглушки и экспериментальные CSS-настройки, не предназначенные для продакшена.

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import { useI18n } from "vue-i18n";
-
-import { setLocale } from "./i18n";
-const { locale } = useI18n();
-
-async function switchLocale() {
-  const newLocale = locale.value === "de" ? "ru" : "de";
-  await setLocale(newLocale);
-}
+import SwitchLocale from "./components/SwitchLocale.vue";
 </script>
 
 <template>
-  <div>
-    <p>{{ $t("hello") }}</p>
-    <button @click="switchLocale">Switch language</button>
-  </div>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <SwitchLocale />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
