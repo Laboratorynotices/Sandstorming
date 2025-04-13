e73db45bf81fa2cc9f246e73c4627b4451630def
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-04-13 17:02:33 +0200

    feat(header): Реализация SEO-оптимизированного заголовка с логотипом
    Добавлен компонент HeaderTitle для отображения заголовка сайта.
    Создан CSS-класс visually-hidden для скрытия контента от пользователей, сохраняя его доступным для SEO.
    Интегрирован новый компонент в шапку сайта.

---
+++ src/components/SiteHeader/HeaderTitle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img
+    src="../../assets/images/header-title.png"
+    alt="Sandstorming"
+    class="logo"
+  />
+  <h1 class="visually-hidden">Sandstorming school</h1>
+</template>
+
+<style lang="css" scoped>
+img {
+  margin: clamp(1.5rem, 3vw, 5rem) auto;
+}
+</style>
--- src/components/SiteHeader/SiteHeader.vue
+++ src/components/SiteHeader/SiteHeader.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import ContainerNav from "./ContainerNav.vue";
+import HeaderTitle from "./HeaderTitle.vue";
 </script>
 
 <template>
   <header>
     <div class="wrap-container">
       <ContainerNav />
+      <HeaderTitle />
+
       SiteHeader
     </div>
   </header>
