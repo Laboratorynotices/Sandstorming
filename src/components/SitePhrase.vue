88ee96ab5f28deb2f5d20f97f6e6ad99f2e97c73
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2025-04-15 11:05:54 +0200

    feat(quotes): добавлен блок с цитатами, отображаемыми в случайном порядке

--- src/components/SitePhrase.vue
+++ src/components/SitePhrase.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+// Список фраз
+const phrases = [
+  "FEAR IS JUST THE WIND BEFORE YOU FLY.",
+  "THE DUNE YOU DREAD TODAY WILL CARRY YOU TOMORROW.",
+  "WHEN YOU RIDE WITH THE STORM, LIMITS FADE AWAY.",
+  "EVERY GRAIN OF SAND KNOWS YOUR POTENTIAL.",
+  "SILENCE THE DOUBT — LET THE DESERT SPEAK.",
+];
+
+// Выбираем случайную фразу из списка
+const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+</script>
+
 <template>
-  <div class="border">SitePhrase (Phrase of the day)</div>
+  <div class="wrap-container">
+    <blockquote class="font-extrabold w-2/3">{{ $t(randomPhrase) }}</blockquote>
+  </div>
 </template>
+
+<style lang="css" scoped>
+blockquote {
+  font-size: clamp(20px, 4.5vw, 64px);
+  margin: clamp(60px, 9vw, 120px) 0;
+}
+</style>
